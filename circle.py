b5bf993acbc09e195e40721be92051203793adea
Ian Flynn <himitsu.monban@gmail.com>
2025-05-30 17:46:33 -0400

    extract circle symmetry into a functio for readability

--- circle.py
+++ circle.py
@@ -9,15 +9,21 @@ point = Point(r, 0)
 def CircleDistance(p: Point, r: int):
     return abs(p.x**2+p.y**2-r**2)
 
+def mirrorPoints(p: Point):
+    return [
+        Point(p.x, p.y),
+        Point(-p.x, p.y),
+        Point(p.x, -p.y),
+        Point(-p.x, -p.y),
+        Point(p.y, p.x),
+        Point(-p.y, p.x),
+        Point(p.y, -p.x),
+        Point(-p.y, -p.x),
+    ]
+    
 while point.y < point.x:
-    b.set(point)
-    b.set(Point(-point.x, point.y))
-    b.set(Point(point.x, -point.y))
-    b.set(Point(-point.x, -point.y))
-    b.set(Point(point.y, point.x))
-    b.set(Point(-point.y, point.x))
-    b.set(Point(point.y, -point.x))
-    b.set(Point(-point.y, -point.x))
+    for p in mirrorPoints(point):
+        b.set(p)
 
     p1 = Point(point.x, point.y+1)
     p2 = Point(point.x-1, point.y+1)
